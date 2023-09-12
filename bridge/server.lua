3a6c1f678394c6eebf3ebbcd32c775a4cf0b20eb
RanDXDev <ranran.al@proton.me>
2023-09-12 16:39:00 +0000

    bridge

---
+++ bridge/server.lua
@@ -0,0 +1,20 @@
+Functions = {}
+
+if not Framework then
+    return error('No framework detected')
+end
+
+local scriptPath = ('bridge/%s/server.lua'):format(Framework)
+local resourceFile = LoadResourceFile(cache.resource, scriptPath)
+
+if not resourceFile then
+    return error(("Unable to find framework bridge for '%s'"):format(Framework))
+end
+
+local func, err = load(resourceFile, ('@@%s/%s'):format(cache.resource, scriptPath))
+
+if not func or err then
+    return error(err)
+end
+
+func()
